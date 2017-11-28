277c8c5803fb84ac0bb21d2dd2e839a823e1046a
bngzl <bngzl@yahoo.com.sg>
2017-11-28 15:39:14 +0000

    Qn 1a done

--- PCA.m
+++ PCA.m
@@ -1,9 +1,15 @@
 % Question 1 
 % Compute mean face 
+load partitionedData.mat 
 N = 52*8; 
 sum_x_train = x_train * ones([416,1]); 
 mean_face = sum_x_train.*(1/N); 
 
 % Compute covariance matrix S 
 A = x_train - mean_face*ones([1,416]); 
-S = A*1/N
+S = (1/N)*(A*A'); 
+[V,D] = eig(S); % A*V = V*D
+evals = diag(D); 
+
+% No. of zero eigenvalues: 
+no_of_zero_evals = size(find(evals));  
