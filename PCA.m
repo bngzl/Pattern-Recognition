7fc03ec5a9da3c8a48a0765421b197dfed1ecedd
bngzl <bngzl@yahoo.com.sg>
2017-11-28 21:40:05 +0000

    Qn1 completed

--- PCA.m
+++ PCA.m
@@ -1,15 +1,29 @@
 % Question 1 
+% Initialisation
+clear all;
+load partitionedData.mat; 
+[D, N] = size(x_train); % D=W*H of each image, N is no. of samples
+
 % Compute mean face 
-load partitionedData.mat 
-N = 52*8; 
-sum_x_train = x_train * ones([416,1]); 
+sum_x_train = x_train * ones([N,1]); 
 mean_face = sum_x_train.*(1/N); 
 
 % Compute covariance matrix S 
-A = x_train - mean_face*ones([1,416]); 
+A = x_train - mean_face*ones([1,N]); 
 S = (1/N)*(A*A'); 
-[V,D] = eig(S); % A*V = V*D
-evals = diag(D); 
+[u,evals] = eig(S); % A*u = u*D
+evals_u = diag(evals); 
+u = normc(u); % Normalise eigenvectors 
 
 % No. of zero eigenvalues: 
-no_of_zero_evals = size(find(evals));  
+% no_of_zero_evals = size(find(evals));  
+
+% Compute (1/N)ATA (PCA when D >> N) 
+S2 = (1/N)*(A'*A); 
+[v,evals] = eig(S2); 
+evals_v = diag(evals); 
+v = normc(v); 
+
+% The M evals_v of ATA correspond to the M largest evals_u of AAT
+
+% Accuracy vs number of eigenvector plot 
