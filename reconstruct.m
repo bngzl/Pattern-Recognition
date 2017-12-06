076629c857f203e588f55b3c329f4bde9134bcc8
bngzl <bngzl@yahoo.com.sg>
2017-12-06 17:20:20 +0000

    Generalised reconstruct function

--- reconstruct.m
+++ reconstruct.m
@@ -1,6 +1,5 @@
-function[x_estimate] = reconstruct(M_pca, nTrainSamples, x_normalised, Sf, x_mean)
-% sample i 
-[u_m, ~] = eigs(Sf, M_pca); 
-w = (x_normalised'*u_m)';
-x_estimate = x_mean*ones(1,nTrainSamples) + (w'*u_m')'; 
+function[x_estimate] = reconstruct(u_m, nSamples, normalised, mean)
+% sample i  
+w = (normalised'*u_m)';
+x_estimate = mean*ones(1,nSamples) + (w'*u_m')'; 
 end
