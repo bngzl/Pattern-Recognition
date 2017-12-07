4260cf187925f242eb6629a5d79976ebb38546c5
bngzl <bngzl@yahoo.com.sg>
2017-12-07 18:35:54 +0000

    doPCA function added

---
+++ doPCA.m
@@ -0,0 +1,12 @@
+function[eigenfaces, eigenvalues]=doPCA(X_train_dataset, M_pca, nTrainSamples)
+x_mean = mean(X_train_dataset, 2); 
+X_normalised = X_train_dataset - x_mean * ones (1, nTrainSamples);
+
+% Cov S for ATA 
+St = X_normalised' * X_normalised ./double(nTrainSamples); 
+[v, Dv] = eigs(St, M_pca); 
+eigenfaces = normc(X_normalised*v); 
+eigenvalues = diag(Dv); 
+end
+
+
